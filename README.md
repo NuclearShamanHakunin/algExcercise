d93a1df8e0c3d85d71d50e9766673db9a8bb1dcb
Marko Jovanovic <marko.jov101@live.com>
2018-03-21 20:03:36 +0100

    1. solved

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # algExcercise
+
+problems found at:
+
+https://techiedelight.quora.com/500-Data-Structures-and-Algorithms-practice-problems-and-their-solutions
---
+++ pairSum.py
@@ -0,0 +1,26 @@
+#Find pair with given sum in the array
+
+niz1 = [12,5,2,7,2,11,3]
+niz2 = [2,7,14,8,6,10,-4]
+
+suma1 = 10
+suma2 = 17
+
+def nadjiParoveSaSumom(niz,suma):
+    tmpDict={}
+    for i in range(len(niz)):
+        for j in range(len(niz)-1):
+            if i==j:
+                continue
+            if niz[i]+niz[j]==suma:
+                if niz[j] in tmpDict:
+                    continue
+                else:
+                    tmpDict.update({niz[i]:niz[j]})
+    return tmpDict
+
+
+a=nadjiParoveSaSumom(niz1,suma1);print(a)
+b=nadjiParoveSaSumom(niz2,suma1);print(b)
+c=nadjiParoveSaSumom(niz1,suma2);print(c)
+d=nadjiParoveSaSumom(niz2,suma2);print(d)
